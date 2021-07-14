4b485a8d5bca3499fbe60dddaf05b6d34ecff43f
Solo <87192036+developer-solo@users.noreply.github.com>
2021-07-14 14:43:31 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_script 'bb_c.lua'
+server_script 'bb_s.lua'
+
+files {
+	"bb_index.html",
+	"bb_js.js",
+	"img/*.png"
+}
+
+ui_page {
+	'bb_index.html',
+}
